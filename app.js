9ebba7c20480e286acad7c32d3deb904a4091c11
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-02-09 19:57:02 -0800

    added question feature

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 //     }
 //     console.log('==========');
 //     //is the same as this....
-//     profileDataArr.forEach((progileItem) => {
+//     profileDataArr.forEach((profileItem) => {
 //         console.log(profileItem)
 //     });
 // };
@@ -21,12 +21,24 @@
 
 // //other option to rewrite function above
 // profileDataArr.forEach(profileItem => console.log(profileItem))
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-const generatePage = require('./src/page-template');
-fs.writeFile('./index.html', generatePage(name, github), err =>{
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
+// LESSON 9.3 CODE TO BE COMMENTED OUT
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+// const pageHTML = generatePage(name, github);
 
-})
+inquirer
+.prompt([{
+    type:'input',
+    name:'name',
+    message:'What is your name?'
+
+}
+])
+.then(answers => console.log(answers));
+
+// fs.writeFile('./index.html', generatePage(name, github), err =>{
+//     if (err) throw err;
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+
+// })
--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,5 @@
 module.exports = generatePage;
+
 const generatePage = (userName, githubName) => {
     return `
     <!DOCTYPE html> 
